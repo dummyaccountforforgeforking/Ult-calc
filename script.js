036dd66484cc852a9fdbe7de84d26cfa9b6e700c
Rocc <140554598+dummyaccountforforgeforking@users.noreply.github.com>
2025-11-24 17:23:04 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+(function () {
+  var D20 = parseInt(document.getElementById("D20").value);
+  var constMod = parseInt(document.getElementById("constMod").value);
+  var strengthMod = parseInt(document.getElementById("strengthMod").value);
+  var dexMod = parseInt(document.getElementById("dexMod").value);
+  var charMod = parseInt(document.getElementById("charMod").value);
+  var wisMod = parseInt(document.getElementById("wisMod").value);
+  var intMod = parseInt(document.getElementById("intMod").value);
+  var diceNum = document.getElementById("diceNum");
+
+  dicenum.Text = toString(((D20 * 3)/2 + constMod + strengthMod + dexMod + charMod + wisMod + intMod) * 0.75)
+
+}
